7adf6201fbc75fe193e5a91e43310f18b67c6c5e
NakkiYANG <NakkiYANG@protonmail.com>
2026-03-21 15:06:21 +0000

    NakkiYANG submits again

--- NakkiYANG/day20_GoldThief.sol
+++ NakkiYANG/day20_GoldThief.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IVault {
+function deposit() external payable;
+function vulnerableWithdraw() external;
+function safeWithdraw() external;
+}
+
 contract GoldThief {
     GoldVault public vault;
     uint256 public attackCount;
